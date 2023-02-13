50cd33259bf4d7e9daa8264544cbf4f5af5cecff
iTaghoury <idriss.taghoury@gmail.com>
2023-02-13 16:16:55 +0100

    added table creation

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+/**
+ * Crée un tableau de longueur n
+ * @param {Number} n 
+ */
 
+function createTab(n) {
+  let tab = Array(n);
+  for(let i = 0; i < n; i++) {
+    tab[i] = Math.floor(100 * Math.random());
+  }
+  return tab;
+}
