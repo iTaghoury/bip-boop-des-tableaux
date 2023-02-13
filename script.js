b063a1c1d1ebaddbef57ed4cb73f922f45ebb76d
iTaghoury <idriss.taghoury@gmail.com>
2023-02-13 16:33:26 +0100

    Added table merging

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /**
- * Crée un tableau de longueur n
+ * Crée et renvoie un tableau de longueur n
  * @param {Number} n 
  */
 
@@ -10,3 +10,33 @@ function createTab(n) {
   }
   return tab;
 }
+
+/**
+ * Crée et renvoie la fusion de tab1 et tab2
+ * @param {Number[]} tab1 
+ * @param {Number[]} tab2 
+ */
+function mergeTables(tab1, tab2) {
+  let i1 = 0;
+  let i2 = 0;
+  let n1 = tab1.length;
+  let n2 = tab2.length;
+  let tab = [];
+  for(let i = 0; i < (n1+n2); i++) {
+    if(tab1[i1] <= tab2[i2]) {
+      tab[i] = tab1[i1];
+      i1++;
+    } else {
+      tab[i] = tab2[i2];
+      i2++;
+    }
+  }
+  return tab;
+}
+
+
+let tab1 = createTab(20);
+let tab2 = createTab(20);
+let tab3 = mergeTables(tab1, tab2);
+
+console.table(tab3);
