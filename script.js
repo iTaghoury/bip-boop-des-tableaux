acae9e30348e2709bc0334f9ae53f86becbbf665
iTaghoury <idriss.taghoury@gmail.com>
2023-02-13 17:01:53 +0100

    changed table merging (now takes sorted arrays and returns a sorted array) + added log for median number

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /**
  * Crée et renvoie un tableau de longueur n
- * @param {Number} n 
+ * @param {Number} n le nombre d'éléments du tableau
  */
 
 function createTab(n) {
@@ -12,24 +12,30 @@ function createTab(n) {
 }
 
 /**
- * Crée et renvoie la fusion de tab1 et tab2
- * @param {Number[]} tab1 
- * @param {Number[]} tab2 
+ * Crée et renvoie la fusion de deux tableaux triés
+ * @param {Number[]} tab1 le premier tableau trié
+ * @param {Number[]} tab2 le second tableau trié
  */
 function mergeTables(tab1, tab2) {
   let i1 = 0;
   let i2 = 0;
   let n1 = tab1.length;
   let n2 = tab2.length;
-  let tab = [];
-  for(let i = 0; i < (n1+n2); i++) {
-    if(tab1[i1] <= tab2[i2]) {
-      tab[i] = tab1[i1];
-      i1++;
-    } else {
-      tab[i] = tab2[i2];
-      i2++;
+  let tab = Array(n1 + n2);
+  for(let i = 0; i < n1; i++) {
+    tab[i] = tab1[i];
+  }
+  for(let i = 0; i < n2; i++) {
+    let j = n1 - 1;
+    for(; j >= 0; j--) {
+      if(tab[j] > tab2[i]) {
+        tab[j+1] = tab[j];
+      } else {
+        break;
+      }
     }
+    n1++;
+    tab[j+1] = tab2[i];
   }
   return tab;
 }
@@ -52,7 +58,11 @@ function sortTable(tab) {
 
 let tab1 = createTab(20);
 let tab2 = createTab(20);
+tab1 = sortTable(tab1);
+tab2 = sortTable(tab2);
 let tab3 = mergeTables(tab1, tab2);
-tab3 = sortTable(tab3);
+//tab3 = sortTable(tab3);
 
 console.table(tab3);
+
+console.log(tab3[tab3.length/2]); // Ou bien console.log(tab3[(tab3.length/2) - 1]);
